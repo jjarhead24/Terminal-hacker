7ed243d759b3d21b8aebe2723b2b654c5291bd97
Jared Seymour <jjarhead24@gmail.com>
2020-06-02 11:13:55 +0100

    updat
    e

--- Terminal hacker/Assets/Hacker.cs
+++ Terminal hacker/Assets/Hacker.cs
@@ -1,18 +1,25 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Hacker : MonoBehaviour
 {
+    //game state
+    int level;
+
+    enum Screen { MainMenu, Password, Win }
+    Screen currentScreen = Screen.MainMenu;
     // Start is called before the first frame update
     void Start()
     {
-        ShowMainMenu("Hi noob");
+        ShowMainMenu();
     }
 
-    void ShowMainMenu(string greeting)
+    void ShowMainMenu()
     {
-        Terminal.WriteLine(greeting);
+        currentScreen = Screen.MainMenu;
+        Terminal.ClearScreen();
         Terminal.WriteLine("Welcome to Terminal Hacker");
         Terminal.WriteLine("Who would you like to hack into? Press the number of your choice");
         Terminal.WriteLine("");
@@ -23,13 +30,52 @@ public class Hacker : MonoBehaviour
         Terminal.WriteLine("Enter your selection:");
     }
 
+
     void OnUserInput(string input)
     {
-        print("The user typed " + input);
-        print(input == "1");
-        //if input == "1";
-        //{
+        if (input == "menu")
+        {
+            ShowMainMenu();
+        }
+        else if (currentScreen == Screen.MainMenu)
+        {
+            RunMainMenu(input);
+        }
 
-        //}
+    }
+
+    private void RunMainMenu(string input)
+    {
+        if (input == "1")
+        {
+            level = 1;
+            StartGame();
+        }
+        else if (input == "2")
+        {
+            level = 2;
+            StartGame();
+        }
+        else if (input == "3")
+        {
+            level = 3;
+            StartGame();
+        }
+        else if (input == "69")
+        {
+            Terminal.WriteLine("uwu");
+        }
+        else
+        {
+            Terminal.WriteLine("please enter a valid level");
+        }
+    }
+
+    void StartGame()
+    {
+        currentScreen = Screen.Password;
+        Terminal.WriteLine("you selected level " + level);
+        Terminal.WriteLine("please enter your password: ");
+
     }
 }
